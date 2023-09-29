e0231e9ecea1dd6ec35bae399cfe77fac63c84aa
ryland <ryland@MacBook-Pro-4.local>
2023-09-28 22:27:58 -0700

    made post route for folders and allowed it to add to user

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,17 +1,30 @@
 const Folder = require('../models/folder')
+const User = require('../models/user')
 const express = require('express')
 const router = express.Router()
 
 //SEED
 
 //INDEX
-router.get('/', (req, res)=>{
-    res.send('testing from the folder route')
+router.get('/', async (req, res)=>{
+    const folders = await Folder.find({})
+    res.json(folders)
 })
 
 //DELETE
 //UPDATE
 //CREATE
+router.post("/", async (req, res) => {
+    try {
+        const newFolder = await Folder.create(req.body)
+        const newFolderId = newFolder._id.toHexString()
+        // await User.findByIdAndUpdate('req.session.userid', {$push: {folders: newFolderId}},{new:true})
+        await User.findByIdAndUpdate('6515dc9ffc1ca272ca121d28', {$push: {folders: newFolderId}},{new:true})
+        res.json(newFolder);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+});
 //SHOW
 
 
