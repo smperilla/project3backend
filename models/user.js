23e0911e77350849dbd0c569b9a939191ed913e7
ryland <ryland@MacBook-Pro-4.local>
2023-10-03 13:30:06 -0700

    can now create new chats

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema(
     {
         username: String,
         password: String,
-        folders: [{ref: 'Folder', type: mongoose.Schema.Types.ObjectId }]
+        folders: [{ref: 'Folder', type: mongoose.Schema.Types.ObjectId }],
+        drafts: [{ref: 'Draft', type: mongoose.Schema.Types.ObjectId }]
     },
     {timestamps: true}
 )
