3e56270dc7126959f4e3257ac9194cdec4d236b5
ryland <ryland@MacBook-Pro-4.local>
2023-10-03 19:47:01 -0700

    initiated sockets

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+
 const cors = require('cors')
 const morgan = require('morgan')
 const PORT = process.env.PORT
@@ -9,6 +12,12 @@ const chatController = require('./controllers/chatController.js')
 const authController = require('./controllers/authController.js')
 //MIDDLEWARE
 app.use(cors())
+const io = new Server(server,{
+    cors: {
+      origin: '*',
+      methods: ["GET", "POST"]
+    }
+  });
 app.use(morgan('tiny'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -18,10 +27,18 @@ app.get('/', (req, res)=>{
     res.send('testing!')
 })
 
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+})
+
+  
 app.use('/users', authController)
 app.use('/folders', folderController)
 app.use('/chats', chatController)
 
-app.listen(PORT, ()=>{
-    console.log(`hello from port: ${PORT}`);
-})
+// app.listen(PORT, ()=>{
+//     console.log(`hello from port: ${PORT}`);
+// })
+server.listen(PORT, ()=>{
+    console.log('hello from socket on port:', PORT);
+});
